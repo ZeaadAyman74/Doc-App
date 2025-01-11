7e46df591ebdca929248c5c374d089b23ad701b3
ziad <zeaadayman92@gmai.com>
2025-01-11 18:03:57 +0200

    - link with firebase - android fastlane - android github actions work flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:advanced_app/app/doc_app.dart';
 import 'package:advanced_app/bloc_observer.dart';
 import 'package:advanced_app/core/di/dependency_injection.dart';
-import 'package:advanced_app/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:advanced_app/firebase_options.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
