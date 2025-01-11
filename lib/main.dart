95e278be560ec53a272bf5c6a63aff3ded05d072
ziad <zeaadayman92@gmai.com>
2025-01-11 17:47:03 +0200

    - link with firebase - android fastlane - android github actions work flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:advanced_app/app/doc_app.dart';
 import 'package:advanced_app/bloc_observer.dart';
 import 'package:advanced_app/core/di/dependency_injection.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   ServiceLocator().init();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   Bloc.observer = MyBlocObserver();
   runApp(const DocApp());
 }
